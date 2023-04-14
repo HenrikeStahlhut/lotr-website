ca7050bc8cdc7368327f66c0f2b5c0570cea747d
Henrike Stahlhut <henrike.st@icloud.com>
2023-04-14 12:58:32 +0200

    add content of all volumes

--- pages/volumes/the-fellowship-of-the-ring.js
+++ pages/volumes/the-fellowship-of-the-ring.js
@@ -1,3 +1,26 @@
+import { volumes } from "../../resources/lib/data.js";
+import Link from "next/link";
+
 export default function VolumeOne() {
-  return <h2>The Fellowship of the Rings</h2>;
+  const findVolume = volumes.find(
+    ({ slug }) => slug === "the-fellowship-of-the-ring"
+  );
+
+  return (
+    <>
+      <p>
+        <Link href="/volumes"> ⬅ All Volumes </Link>
+      </p>
+      <h1>{findVolume.title}</h1>
+      <p>{findVolume.description}</p>
+      <ul>
+        <li>
+          {findVolume.books[0].ordinal} - {findVolume.books[0].title}
+        </li>
+        <li>
+          {findVolume.books[1].ordinal} - {findVolume.books[1].title}
+        </li>
+      </ul>
+    </>
+  );
 }
--- pages/volumes/the-return-of-the-king.js
+++ pages/volumes/the-return-of-the-king.js
@@ -1,3 +1,26 @@
-export default function VolumeThree() {
-  return <h2>The return of the King</h2>;
+import { volumes } from "../../resources/lib/data.js";
+import Link from "next/link";
+
+export default function VolumeOne() {
+  const findVolume = volumes.find(
+    ({ slug }) => slug === "the-return-of-the-king"
+  );
+
+  return (
+    <>
+      <p>
+        <Link href="/volumes"> ⬅ All Volumes </Link>
+      </p>
+      <h1>{findVolume.title}</h1>
+      <p>{findVolume.description}</p>
+      <ul>
+        <li>
+          {findVolume.books[0].ordinal} - {findVolume.books[0].title}
+        </li>
+        <li>
+          {findVolume.books[1].ordinal} - {findVolume.books[1].title}
+        </li>
+      </ul>
+    </>
+  );
 }
